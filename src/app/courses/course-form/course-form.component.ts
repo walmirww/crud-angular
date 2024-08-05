3f8c6a3ce67fb5c978960f9aa9155ef4f6df9c02
Walmir Wild <walmirww@gmail.com>
2024-08-05 08:54:36 -0300

    Adjust before Update Angular

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,3 @@
-import { error } from 'console';
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
