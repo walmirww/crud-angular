823c4061a55516218be4cf395c3d83561f07f42f
Walmir Wild <walmirww@gmail.com>
2024-08-05 08:23:39 -0300

    Criar curso + POST

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,5 @@
+import { error } from 'console';
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,7 +17,8 @@ export class CourseFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
     ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -28,11 +31,16 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value)
-      .subscribe(result => console.log(result), error => this.onError());
+      .subscribe(result => this.onSuccess(), error => this.onError());
   }
 
   onCancel() {
-    console.log('onCancel');
+    this.location.back();
+  }
+
+  onSuccess() {
+    this.snackBar.open('Curso salvo com Sucesso', '', {duration: 5000});
+    this.onCancel();
   }
 
   onError() {
