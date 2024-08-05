762c951fb7fe4ff86ab373e7204251f751c576ed
Walmir Wild <walmirww@gmail.com>
2024-08-05 06:34:34 -0300

    Formulario Criar curso

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-course-form',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-form.component.scss']
 })
 export class CourseFormComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      name: [null],
+      category: [null]
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit() {
+
+  }
+
+  onCancel() {
+
   }
 
 }
